a5ea6580521b718b1efc677f7139ac15c376751c
Schildorfer <schildor@napier.cs.grinnell.edu>
2020-02-08 15:56:33 -0600

    Finish Chapter 3.4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Hello App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Hello App"
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Hello App"
+  end
+  
 
 end
