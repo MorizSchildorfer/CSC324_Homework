cbad084d76dbda7198d376397605c0d05a044a6a
Schildorfer <schildor@baldwin.cs.grinnell.edu>
2020-02-14 15:32:04 -0600

    Dynamic Random Image

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,10 +2,11 @@ require 'chunky_png'
 class ImageGen
 	def self.gen()
 		filename = 'app/assets/images/filename.png'
+		r = (1..4).to_a.shuffle.first
            	png = ChunkyPNG::Image.new(32, 32, ChunkyPNG::Color::TRANSPARENT)
 		for i in (0..31)
 			for j in (0..31)
-				grey = mapTo255(i+j, 0, 64)
+				grey = mapTo255(i+j, 0, 64)*r
 				png[j,i] = ChunkyPNG::Color.rgba(grey, grey, grey, 255)
 			end
 		end
