07f9b65a526d9fd4775f3ed6c71add72e09fe04a
Schildorfer <schildor@demorgan.cs.grinnell.edu>
2020-02-17 21:45:07 -0600

    Sine waves

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,29 +2,27 @@ require 'chunky_png'
 
 class ImageGen
 	def self.gen()
-		dir = Rails.root.join('tmp').to_s
 		r = (1..4).to_a.shuffle.first
-		#filename = dir.dup.concat('/image.png')
-		#puts filename
-           	png = ChunkyPNG::Image.new(64, 64, ChunkyPNG::Color::TRANSPARENT)
-		for i in (0..63)
-			for j in (0..63)
+           	png = ChunkyPNG::Image.new(400, 400, ChunkyPNG::Color::TRANSPARENT)
+		for i in (0..399)
+			for j in (0..399)
 				#i*j+j*i
 				#i*i+j*j
 				#i*j+j*j
 				#i*j+i*i
 				#i*j
 				#i+j
-				grey = (mapTo255(i*j+j*j, 0, 63)*r)%255
+				#(mapTo255(i*j+j*i, 0, 255)*r)%255
+				grey = (mapTo255(Math.sin(i/(r*Math::PI))*Math.cos(j/(r*Math::PI)), -1, 1))%255
 				png[j,i] = ChunkyPNG::Color.rgba(grey, grey, grey, 255)
 			end
 		end
-		# png.save(filename, :interlace => true)
 		return [png.resize(400,400), r]
 		
 	end
 
 	def self.mapTo255(input, max, min)
-		return ((((input.to_f)+min)/(min+max))*255).to_i
+		val = ((((input.to_f)-min)/(max-min))*255)
+		return val.to_i
 	end
 end
