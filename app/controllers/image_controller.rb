69b4b42edffd407ec35d536c2b5c3f46c683a88f
Schildorfer <schildor@codd.cs.grinnell.edu>
2020-02-19 16:11:59 -0600

    HW 4

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,10 +2,13 @@ require 'chunky_png'
 
 class ImageGen
 	def self.gen()
+		a= Hash.new(0)
+		dim=400
 		r = (1..4).to_a.shuffle.first
-           	png = ChunkyPNG::Image.new(400, 400, ChunkyPNG::Color::TRANSPARENT)
-		for i in (0..399)
-			for j in (0..399)
+           	png = ChunkyPNG::Image.new(dim, dim, ChunkyPNG::Color::TRANSPARENT)
+		max=min=Math.sin(0/(r*Math::PI))+Math.cos(0/(r*Math::PI))
+		for i in (0..(dim-1))
+			for j in (0..(dim-1))
 				#i*j+j*i
 				#i*i+j*j
 				#i*j+j*j
@@ -13,8 +16,21 @@ class ImageGen
 				#i*j
 				#i+j
 				#(mapTo255(i*j+j*i, 0, 255)*r)%255
-				grey = (mapTo255(Math.sin(i/(r*Math::PI))+Math.sin(j/(3*r*Math::PI))*Math.cos(j/(0.75*r*Math::PI)), -2, 2))%255
-				png[j,i] = ChunkyPNG::Color.rgba(grey, grey, grey, 255)
+				v= Math.sin(i/(r*Math::PI))+Math.cos(j/(2*r*Math::PI))*Math.cos(j/(0.75*r*Math::PI))
+				if(v>max) 
+					max=v 
+				elsif(v<min) 
+					min=v 
+				end
+				a[[j,i]]= v
+			end
+		end
+		for i in (0..(dim-1))
+			for j in (0..(dim-1))
+				v= a[[j,i]]
+				grey = (mapTo255(v, max, min))%255
+				colorArr = calcColorGrad(grey)
+				png[j,i] = ChunkyPNG::Color.rgba(colorArr[0], colorArr[1], colorArr[2], 255)
 			end
 		end
 		return [png.resize(400,400), r]
@@ -25,4 +41,20 @@ class ImageGen
 		val = ((((input.to_f)-min)/(max-min))*255)
 		return val.to_i
 	end
+	def self.calcColorGrad(grey)
+		arr = Array.new(3)
+		if(grey<64)
+			arr=[255,grey*4,0]
+		elsif(grey<128)
+			grey= grey-64
+			arr=[255-grey*4,255,0]
+		elsif(grey<192)
+			grey= grey-128
+			arr=[0,255,grey*4]
+		else
+			grey= grey-192
+			arr=[0,255-grey*4,255]
+		end
+	end
+			
 end
