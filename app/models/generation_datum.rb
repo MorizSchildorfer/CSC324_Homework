9b7100e9f675e27c33e1fcd8647c41f13cc7a7f2
Schildorfer <schildor@codd.cs.grinnell.edu>
2020-03-04 15:04:53 -0600

    GenerationData Table added and restraints tested

--- app/models/generation_datum.rb
+++ app/models/generation_datum.rb
@@ -1,4 +1,5 @@
 class GenerationDatum < ApplicationRecord
+  validates :seed, presence: true, numericality: {only_integer: true}
   validates :width, presence: true, numericality: {greater_than: 0, only_integer: true}
   validates :height, presence: true, numericality: {greater_than: 0, only_integer: true}
   validates :river_length, presence: true, numericality: {greater_than: -1, only_integer: true}
--- test/models/generation_datum_test.rb
+++ test/models/generation_datum_test.rb
@@ -8,9 +8,28 @@ class GenerationDatumTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @dat.valid?
   end
-
+  test "seed should be integer" do
+    @dat.seed = -5.5
+    assert_not @dat.valid?
+  end
   test "width should be positive" do
     @dat.width = -5
     assert_not @dat.valid?
+    @dat.width = 0
+    assert_not @dat.valid?
+  end
+
+  test "height should be positive" do
+    @dat.height = -5
+    assert_not @dat.valid?
+    @dat.height = 0
+    assert_not @dat.valid?
+  end
+
+  test "river length should be non-negative" do
+    @dat.river_length = -5
+    assert_not @dat.valid?
+    @dat.river_length = 0
+    assert @dat.valid?
   end
 end
