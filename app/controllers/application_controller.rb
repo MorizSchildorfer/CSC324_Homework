354dbd93b104a665ed093ddf899c65a20ac3b6a2
MorizSchildorfer <MorizSchildorfer@users.noreply.github.com>
2020-07-07 22:40:39 -0500

    Github bug check
    I cant push on a different repo so I am testing if the issue persists here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hyello you stinky lizard. Go eat a fly, ugly."
+    render html: "Hyello you stinky lizard. Go eat a fly, ugly."   
   end
 
 end
